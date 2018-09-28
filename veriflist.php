14303f2b72c3faacfa38a490cd52a079028ce110
Jessy Fouace <jessy.fouace62@gmail.com>
2018-09-28 11:57:38 +0200

    add animation on project index

--- veriflist.php
+++ veriflist.php
@@ -3,7 +3,7 @@ session_start();
 require('config.php');
 if (!empty($_SESSION['id'])) {
   if (!empty($_POST['list_name'])) {
-    $taskname = htmlspecialchars($_POST['list_name']);
+    $taskname = htmlspecialchars(addslashes(strip_tags($_POST['list_name'])));
     $newlist = $bdd->prepare("INSERT INTO lists (list, id_project, id_creator) VALUES (:list, :idproject, :idcreator)");
     $newlist->execute(array(
       'list' => $taskname,
